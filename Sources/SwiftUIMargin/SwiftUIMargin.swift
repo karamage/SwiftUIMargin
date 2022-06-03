8f0d354a2b27131895868258a7d7c3c019e71040
karamage <karamage.umauma@gmail.com>
2022-06-03 15:55:32 +0900

    add margin modifier.

--- Sources/SwiftUIMargin/SwiftUIMargin.swift
+++ Sources/SwiftUIMargin/SwiftUIMargin.swift
@@ -1,6 +1,24 @@
-public struct SwiftUIMargin {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+struct MarginModifier: ViewModifier {
+    var top = 0.0
+    var bottom = 0.0
+    var left = 0.0
+    var right = 0.0
+    
+    func body(content: Content) -> some View {
+        VStack {
+            Spacer()
+                .frame(height: top)
+            HStack {
+                Spacer()
+                    .frame(width: left)
+                content
+                Spacer()
+                    .frame(width: right)
+            }
+            Spacer()
+                .frame(height: bottom)
+        }
     }
 }
--- Tests/SwiftUIMarginTests/SwiftUIMarginTests.swift
+++ Tests/SwiftUIMarginTests/SwiftUIMarginTests.swift
@@ -6,6 +6,6 @@ final class SwiftUIMarginTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUIMargin().text, "Hello, World!")
+        // XCTAssertEqual(SwiftUIMargin().text, "Hello, World!")
     }
 }
